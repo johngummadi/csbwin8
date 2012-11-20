768412bc4c9e85be5008f90d3c6917b6d4abf980
John K Gummadi <johngummadi@gmail.com>
2012-11-19 19:01:11 -0800

    First commit

---
+++ js/data.js
@@ -0,0 +1,41 @@
+﻿var gBooks = new Books();
+
+function Books() {
+    this.mBooks = null;
+
+    this.load = function (loadedBooksEvent, errorEvent) {
+        var httpReq = new XMLHttpRequest();
+        var url = "http://cloudstudybible.com/php/getbooksjson.php";
+        httpReq.open("GET", url, true);
+        httpReq.onreadystatechange = (function stateChanged() {
+            if (this.readyState == 4) {
+                var booksContainer = JSON.parse(httpReq.responseText);
+                gBooks.mBooks = booksContainer.Books;
+                if (loadedBooksEvent != null)
+                    loadedBooksEvent(gBooks.mBooks);
+            }
+        });
+        httpReq.send(null);
+    } //load()
+    
+    this.getBooks = function () {
+        return this.mBooks;
+    } //getBooks()
+} //Books
+
+function Chapter() {
+    
+    this.load = function (bookId, chapterNumber, loadedChapterEvent, errorEvent) {
+        var httpReq = new XMLHttpRequest();
+        var url = "http://cloudstudybible.com/php/getchapterjson.php?b=" + chapterNumber + "&c=" + chapterNumber + "";
+        httpReq.open("GET", url, true);
+        httpReq.onreadystatechange = (function stateChanged() {
+            if (this.readyState == 4) {
+                var chapter = JSON.parse(httpReq.responseText);
+                if (loadedChapterEvent != null)
+                    loadedChapterEvent(chapter);
+            }
+        });
+        httpReq.send(null);
+    } //load()
+}
